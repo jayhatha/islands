d46bb091f189f587cfadaa9b989544bb28eb9aec
jay-foundry <jay@foundryinteractive.com>
2020-04-25 09:10:10 -0700

    add color palettes and eslint config

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,75 @@
 import '../styles/index.scss';
-import { random, randomBreak, randomAdditive, seedIslands, findAdjacent } from './methods';
+import { seedIslands } from './methods';
 
 // elements
+
 const container = document.getElementById('container');
 const regenButton = document.getElementById('regen');
-// consts
-const initialColors = ['#FBF8F5', '#AA995B', '#87A79B', '#CDE1DC', '#9E919D' ];
+
+const growthInput = document.getElementById('growthFactor');
+const iterationInput = document.getElementById('iterations');
+const islandCountInput = document.getElementById('islandCount');
+const paletteSelect = document.getElementById('palette');
+
+// palettes
+const palettes = {
+  standard: ['#FBF8F5', '#AA995B', '#87A79B', '#CDE1DC', '#9E919D'],
+  moss: [
+    '#cad2c5ff',
+    '#84a98cff',
+    '#52796fff',
+    '#354f52ff',
+    '#2f3e46ff',
+  ],
+  pastel: [
+    '#9c89b8ff',
+    '#f0a6caff',
+    '#efc3e6ff',
+    '#f0e6efff',
+    '#b8beddff',
+  ],
+  sunset: ['#eeaf61', '#fb9062', '#ee5d6c', '#ce4993', '#6a0d83'],
+};
+
 // config
-let makeGrid = seedIslands;
-// helpers
+
+const config = {
+  gridSize: 4096,
+  growthFactor: 10,
+  iterations: 7,
+  islandCount: 7,
+  palette: palettes.standard,
+  container,
+};
+const makeGrid = seedIslands;
+
+// handlers
+const updateGrowthValue = (e) => {
+  config.growthFactor = e.target.value;
+};
+const updateIterationValue = (e) => {
+  config.iterations = e.target.value;
+};
+const updateIslandCount = (e) => {
+  config.islandCount = e.target.value;
+};
+const updatePalette = (e) => {
+  config.palette = palettes[e.target.value];
+};
 const regenerateGrid = () => {
   while (container.firstChild) {
     container.firstChild.remove();
-}
-  makeGrid(4096, initialColors);
-};;
+  }
+  makeGrid(config);
+};
 
-document.addEventListener("DOMContentLoaded", function() {
+// init
+
+document.addEventListener('DOMContentLoaded', () => {
   regenButton.onclick = () => regenerateGrid();
-  makeGrid(4096, initialColors);
+  makeGrid(config);
+  growthInput.addEventListener('change', updateGrowthValue);
+  iterationInput.addEventListener('change', updateIterationValue);
+  islandCountInput.addEventListener('change', updateIslandCount);
+  paletteSelect.addEventListener('change', updatePalette);
 });
--- src/scripts/methods.js
+++ src/scripts/methods.js
@@ -1,17 +1,17 @@
-import {countBy} from 'lodash';
+// import { countBy } from 'lodash';
 
-const randomColor = (colors) =>  (colors[Math.floor(Math.random()*colors.length)]);
+const randomColor = (colors) =>
+  colors[Math.floor(Math.random() * colors.length)];
 const findSeeds = () => {
-  let seedNums = [];
-  let islands = document.getElementsByClassName('seed');
+  const seedNums = [];
+  const islands = document.getElementsByClassName('seed');
   for (const island of islands) {
-    seedNums.push(parseInt(island.id));
+    seedNums.push(parseInt(island.id, 10));
   }
   return seedNums;
-
 };
 export const findAdjacent = (num) => {
-  // corners 
+  // corners
   if (num === 0) {
     return [1, 64];
   }
@@ -24,95 +24,94 @@ export const findAdjacent = (num) => {
   if (num === 4095) {
     return [4094, 4032];
   }
-  // edges 
-  // top row 
-  if (0 < num && num < 63) {
+  // edges
+  // top row
+  if (num > 0 && num < 63) {
     return [num + 64, num + 1, num - 1];
   }
-  // bottom row 
-  if (4032 < num && num < 4095) {
+  // bottom row
+  if (num > 4032 && num < 4095) {
     return [num - 64, num + 1, num - 1];
   }
-  // left edge 
-  if (num > 63 && num < 4032 && ((num) % 64) === 0) {
+  // left edge
+  if (num > 63 && num < 4032 && num % 64 === 0) {
     return [num + 1, num + 64, num - 64];
   }
   // right edge
-  if (num > 63 && num < 4095 && ((num + 1) % 64) === 0) {
-    return [num - 1, num + 64, num - 64 ];
+  if (num > 63 && num < 4095 && (num + 1) % 64 === 0) {
+    return [num - 1, num + 64, num - 64];
   }
   // default
   return [num + 1, num - 1, num + 64, num - 64];
-}; 
+};
 
 const paintAdjacents = (seedNums) => {
-  let nums = [...seedNums];
+  const nums = [...seedNums];
   let bgColor;
   let adjacents;
   let idx = 0;
   for (const num of nums) {
     adjacents = findAdjacent(num);
     idx += 1;
-    
+
     bgColor = document.getElementById(num).style.backgroundColor;
-      for (const item of adjacents) {
-        let island = document.getElementById(item);
-        island.style.backgroundColor = bgColor;
-        island.classList.add('claimed', idx);
-      }
+    for (const item of adjacents) {
+      const island = document.getElementById(item);
+      island.style.backgroundColor = bgColor;
+      island.classList.add('claimed', idx);
+    }
     adjacents = [];
- ;   }  
+  }
 };
 const isClaimed = (id) => {
-  let el = document.getElementById(id);
-  if (el) {
-  return el.classList.contains('claimed');
-  };
+  const el = document.getElementById(id);
+  return el ? el.classList.contains('claimed') : false;
 };
 
 const findPotentialPaints = () => {
   // get all claimed ids
-  let claimedIds = [];
-  let claimedSquares = document.getElementsByClassName('claimed');
+  const claimedIds = [];
+  const claimedSquares = document.getElementsByClassName('claimed');
   for (const island of claimedSquares) {
-    claimedIds.push(parseInt(island.id));
+    claimedIds.push(parseInt(island.id, 10));
   }
   // find their adjacents
-  let potentiallyPaintable = [];
-  for (const id in claimedIds) {
-    let sourceIsland = document.getElementById(claimedIds[id]);
-    let bgColor = sourceIsland.style.backgroundColor;
-    let adjacents = findAdjacent(claimedIds[id]);   
-    for (const item in adjacents) {
-      potentiallyPaintable.push({id: adjacents[item], backgroundColor: bgColor});      
-      }
-    } 
+  const potentiallyPaintable = [];
+  for (const id of claimedIds) {
+    const sourceIsland = document.getElementById(id);
+    const bgColor = sourceIsland.style.backgroundColor;
+    const adjacents = findAdjacent(id);
+    for (const item of adjacents) {
+      potentiallyPaintable.push({
+        id: item,
+        backgroundColor: bgColor,
+      });
+    }
+  }
   return potentiallyPaintable;
 };
 
 const paintableSquares = () => {
-  let squaresToPaint = [];
-  let candidates = findPotentialPaints();
+  const squaresToPaint = [];
+  const candidates = findPotentialPaints();
   for (const id of candidates) {
     let openBorders = 0;
-    let adjacents = findAdjacent(id.id);
+    const adjacents = findAdjacent(id.id);
     for (const item of adjacents) {
-      
       if (!isClaimed(item.id)) {
         openBorders += 1;
         if (openBorders > 2) {
-        squaresToPaint.push(id);  
+          squaresToPaint.push(id);
         }
-      }    
+      }
     }
   }
   return squaresToPaint;
 };
 
 const paintSquares = (squaresToPaint) => {
-  
   for (const square of squaresToPaint) {
-    let island = document.getElementById(square.id);
+    const island = document.getElementById(square.id);
     if (island && !island.style.backgroundColor) {
       window.setTimeout(1000);
       island.style.backgroundColor = square.backgroundColor;
@@ -121,26 +120,24 @@ const paintSquares = (squaresToPaint) => {
   }
 };
 
-const growIsland = (paintableSquares, growthRate) => {
-  let potentialNewTerritory = paintableSquares;
-  let theExpanse = [];
-    for (let j = 0; j < potentialNewTerritory.length; j += 1) {
-      if (Math.floor(Math.random()*100) < growthRate) {
-        theExpanse.push(potentialNewTerritory[j]);
-      }
+const growIsland = (paintable, growthRate) => {
+  const potentialNewTerritory = paintable;
+  const theExpanse = [];
+  for (let j = 0; j < potentialNewTerritory.length; j += 1) {
+    if (Math.floor(Math.random() * 100) < growthRate) {
+      theExpanse.push(potentialNewTerritory[j]);
     }
-    paintSquares(theExpanse);
+  }
+  paintSquares(theExpanse);
 };
 
-const selectIsland = (islandNumber) => {
+// const selectIsland = (islandNumber) => {};
 
-};
-
-export const random = (num, cols) => {
-  let newItems = [];
-  let colors = [...cols];
+export const random = (num, cols, container) => {
+  const newItems = [];
+  const colors = [...cols];
   for (let i = 0; i < num; i += 1) {
-    let color = randomColor(colors);
+    const color = randomColor(colors);
     newItems.push(document.createElement('div'));
     newItems[i].classList.add('item');
     newItems[i].id = i;
@@ -151,11 +148,30 @@ export const random = (num, cols) => {
   }
 };
 
-export const randomAdditive = (num, cols) => {
-  let newItems = [];
-  let colors = [...cols];
+export const randomAdditive = (num, cols, container) => {
+  const newItems = [];
+  const colors = [...cols];
   for (let i = 0; i < num; i += 1) {
-    let color = randomColor(target);
+    const color = randomColor(colors);
+    newItems.push(document.createElement('div'));
+    newItems[i].classList.add('item');
+    newItems[i].id = i;
+    newItems[i].style.backgroundColor = color;
+    colors.push(color);
+  }
+  for (const el of newItems) {
+    container.appendChild(el);
+  }
+};
+
+export const randomBreak = (num, cols, container) => {
+  const newItems = [];
+  const colors = [...cols];
+  const colors2 = [...cols];
+  for (let i = 0; i < num; i += 1) {
+    const target =
+      i > Math.floor(Math.random() * num) ? colors2 : colors;
+    const color = randomColor(target);
     newItems.push(document.createElement('div'));
     newItems[i].classList.add('item');
     newItems[i].id = i;
@@ -165,56 +181,43 @@ export const randomAdditive = (num, cols) => {
   for (const el of newItems) {
     container.appendChild(el);
   }
+  // console.log(countBy(colors));
+  // console.log(countBy(colors2));
 };
 
-export const randomBreak = (num, cols) => {
-  let newItems = [];
-  let colors = [...cols];
-  let colors2 = [...cols];
-  for (let i = 0; i < num; i += 1) {
-    let target = i > Math.floor(Math.random()*num) ? colors2 : colors;
-    let color = randomColor(target);
-    newItems.push(document.createElement('div'));
-    newItems[i].classList.add('item');
-    newItems[i].id = i;
-    newItems[i].style.backgroundColor = color;
-    target.push(color);
-  }
-  for (const el of newItems) {
-    container.appendChild(el);
-  }
-  console.log(countBy(colors));
-  console.log(countBy(colors2));
-};
-
-export const seedIslands = (num, cols) => {
-  let newItems = [];
-  let colors = [...cols];
-  let seedNumbers = [];
-  for (let s = 0; s < 7; s += 1 ) {
-    let seedNum = Math.floor(Math.random()*num);
+export const seedIslands = (config) => {
+  const {
+    gridSize,
+    palette,
+    growthFactor,
+    islandCount,
+    iterations,
+    container,
+  } = config;
+  const newItems = [];
+  const colors = [...palette];
+  const seedNumbers = [];
+  for (let s = 0; s < islandCount; s += 1) {
+    const seedNum = Math.floor(Math.random() * gridSize);
     seedNumbers.push(seedNum);
   }
-  for (let i = 0; i < num; i += 1) {
-    let color = randomColor(colors);
+  for (let i = 0; i < gridSize; i += 1) {
+    const color = randomColor(colors);
     newItems.push(document.createElement('div'));
     newItems[i].classList.add('item');
     newItems[i].id = i;
     if (seedNumbers.includes(i)) {
-    newItems[i].style.backgroundColor = color;
-    newItems[i].classList.add('claimed', 'seed');
-    };
+      newItems[i].style.backgroundColor = color;
+      newItems[i].classList.add('claimed', 'seed');
+    }
   }
   for (const el of newItems) {
     container.appendChild(el);
   }
-  let x = paintableSquares();
+  let x = paintableSquares(findPotentialPaints());
   paintAdjacents(findSeeds());
-  for (let i = 0; i < 10; i += 1) {
-  growIsland(x, 10);
-  x = paintableSquares(findPotentialPaints());
+  for (let i = 0; i < iterations; i += 1) {
+    growIsland(x, growthFactor);
+    x = paintableSquares(findPotentialPaints());
   }
 };
-
-
-
